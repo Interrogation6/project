1d4a00d87c91300a0807bba078febdd9112de485
Kelvyn Dantas <kelvyndantas@hotmail.com>
2025-10-04 14:03:51 -0300

    init

---
+++ src/test/java/detalhes/prata/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package detalhes.prata.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
